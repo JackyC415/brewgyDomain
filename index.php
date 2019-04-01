9bdd233e931189b5d2adca59823e04bd325dbfa1
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2019-03-31 19:28:13 -0700

    added index page

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+ <!--HTML/CSS templates source code references; credit goes to: 
+  https://html5up.net/phantom
+  https://colorlib.com/wp/template/contact-form-v1/
+  https://www.w3schools.com/w3css/tryw3css_templates_startup.htm
+  https://designmodo.com/shopping-cart-ui/
+  https://teamtreehouse.com/library/-linking-it-all-up
+  https://www.w3schools.com/howto/howto_css_blog_layout.asp-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="website logo" href="images/brewgy.png">
+  <link href="https://fonts.googleapis.com/css?family=Muli%7CRoboto:400,300,500,700,900" rel="stylesheet"></head>
+
+  <body>
+    <div class="main-nav">
+        <ul class="nav">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="About.php" target="_blank">About</a></li>
+          <li><a href="Products.php" target="_blank">Products</a></li>
+          <li><a href="News.php" target="_blank">News</a></li>
+          <li><a href="Contacts.php" target="_blank">Contacts</a></li>
+          <li><a href="register.php" target="_blank">Registration</a></li>
+          <li><a href="user.php" target="_blank">User</a></li>
+        </ul>
+    </div>
+
+    <header>
+      <img src="images/kitty.png" alt="Jack Chen" class="profile-image">
+      <h1 class="tag name">Hello, I’m Jack.</h1>
+      <p class="tag location">Welcome To My Portfolio!</p>
+    </header>
+
+            <h1>Recent Projects<br /></h1>
+
+              <section class="tiles">
+                <article class="style1">
+                  <span class="image">
+                    <img src="images/pic01.jpg" alt="" />
+                  </span>
+                  <a href="index.php">
+                    <h2>THIS</h2>
+                    <div class="content">
+                      <p>Yes, this website.</p>
+                    </div>
+                  </a>
+                </article>
+                <article class="style2">
+                  <span class="image">
+                    <img src="images/pic02.jpg" alt="" />
+                  </span>
+                  <a href="#">
+                    <h2>TBD</h2>
+                    <div class="content">
+                      <p>Not Available Yet.</p>
+                    </div>
+                  </a>
+                </article>
+                <article class="style3">
+                  <span class="image">
+                    <img src="images/pic03.jpg" alt="" />
+                  </span>
+                  <a href="#">
+                    <h2>TBD</h2>
+                    <div class="content">
+                      <p>Not Available Yet.</p>
+                    </div>
+                  </a>
+                </article>
+                  <article class="style4">
+                  <span class="image">
+                    <img src="images/pic04.jpg" alt="" />
+                  </span>
+                  <a href="#">
+                    <h2>TBD</h2>
+                    <div class="content">
+                      <p>Not Available Yet.</p>
+                    </div>
+                  </a>
+                </article>
+                <article class="style5">
+                  <span class="image">
+                    <img src="images/pic05.jpg" alt="" />
+                  </span>
+                  <a href="#">
+                    <h2>TBD</h2>
+                    <div class="content">
+                      <p>Not Available Yet.</p>
+                    </div>
+                  </a>
+                </article>
+                <article class="style6">
+                  <span class="image">
+                    <img src="images/pic06.jpg" alt="" />
+                  </span>
+                  <a href="#">
+                    <h2>TBD</h2>
+                    <div class="content">
+                      <p>Not Available Yet.</p>
+                    </div>
+                  </a>
+                </article>
+    <footer>
+     <ul>
+     <li><a href="https://github.com/JackyC415" target="_blank" class="social github">Github</a></li>
+     <li><a href="https://www.linkedin.com/in/jackyc415/" target="_blank" class="social linkedin">LinkedIn</a></li>
+     <li><a href="https://www.facebook.com/jackyzchen415" target="_blank" class="social facebook">Facebook</a></li>
+     </ul>
+    </footer>
+  </body>
+  </html>
