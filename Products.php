438f0707d4b7bf63926c3c72de314b63b350db93
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2019-04-01 19:58:03 -0700

    added products page

---
+++ Products.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Products</title>
+    <link rel="stylesheet" href="productStyle.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+
+    <!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-card" id="myNavbar">
+    <a href="index.php" class="w3-bar-item w3-button w3-wide">Brewgy</a>
+  </div>
+</div>
+
+    <div class="shopping-cart">
+      <!-- Title -->
+      <div class="title">
+       Products
+      </div>
+
+      <!-- Product #1 -->
+      <div class="item">
+        <div class="buttons">
+          <span class="delete-btn"></span>
+          <span class="like-btn"></span>
+        </div>
+
+        <div class="image">
+          <img src="images/droneProduct.jpg" alt="img" />
+        </div>
+
+        <div class="description">
+          <span>Brewgy X</span>
+          <p></p>
+          <span>Fly speed up to 80mph</span>
+        </div>
+
+        <div class="quantity">
+          <button class="plus-btn" type="button" name="button">
+            <img src="images/plus.svg" alt="" />
+          </button>
+          <input type="text" name="name" value="1">
+          <button class="minus-btn" type="button" name="button">
+            <img src="images/minus.svg" alt="" />
+          </button>
+        </div>
+
+        <div class="total-price">$999</div>
+      </div>
+
+      <!-- Product #2 -->
+      <div class="item">
+        <div class="buttons">
+          <span class="delete-btn"></span>
+          <span class="like-btn"></span>
+        </div>
+
+        <div class="image">
+          <img src="images/droneProduct.jpg" alt="img"/>
+        </div>
+
+        <div class="description">
+          <span>Brewgy Xl</span>
+          <p></p>
+          <span>Fly speed up to 100mph</span>
+        </div>
+
+        <div class="quantity">
+          <button class="plus-btn" type="button" name="button">
+            <img src="images/plus.svg" alt="" />
+          </button>
+          <input type="text" name="name" value="1">
+          <button class="minus-btn" type="button" name="button">
+            <img src="images/minus.svg" alt="" />
+          </button>
+        </div>
+
+        <div class="total-price">$1199</div>
+      </div>
+
+      <!-- Product #3 -->
+      <div class="item">
+        <div class="buttons">
+          <span class="delete-btn"></span>
+          <span class="like-btn"></span>
+        </div>
+
+        <div class="image">
+          <img src="images/droneProduct.jpg" alt="img" />
+        </div>
+
+        <div class="description">
+          <span>Brewgy Xll</span>
+          <p></p>
+          <span>Fly speed up to 150mph</span>
+        </div>
+
+        <div class="quantity">
+          <button class="plus-btn" type="button" name="button">
+            <img src="images/plus.svg" alt="" />
+          </button>
+          <input type="text" name="name" value="1">
+          <button class="minus-btn" type="button" name="button">
+            <img src="images/minus.svg" alt="" />
+          </button>
+        </div>
+
+        <div class="total-price">$1499</div>
+      </div>
+
+      <!-- Product #4 -->
+      <div class="item">
+        <div class="buttons">
+          <span class="delete-btn"></span>
+          <span class="like-btn"></span>
+        </div>
+
+        <div class="image">
+          <img src="images/droneProduct.jpg" alt="img" />
+        </div>
+
+        <div class="description">
+          <span>Drone Xlll</span>
+          <p></p>
+          <span>Fly speed up to 200mph</span>
+        </div>
+
+        <div class="quantity">
+          <button class="plus-btn" type="button" name="button">
+            <img src="images/plus.svg" alt="" />
+          </button>
+          <input type="text" name="name" value="1">
+          <button class="minus-btn" type="button" name="button">
+            <img src="images/minus.svg" alt="" />
+          </button>
+        </div>
+
+        <div class="total-price">$1999</div>
+      </div>
+    </div>
+  </body>
+</html>
