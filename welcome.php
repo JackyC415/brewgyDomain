08c970d234c2db7bf1f82d52e04fdac17db28d3d
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2019-04-01 20:00:27 -0700

    added welcome page

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<h1>Current Users</h1>
+	<ul>
+		<li>Mary Smith</li>
+		<li>John Wang</li>
+		<li>Alex Bington</li>
+		<li>John Smith</li>
+		<li>Peter Li</li>
+		<li>Jason Well</li>
+	</ul>
+</body>
+</html>
